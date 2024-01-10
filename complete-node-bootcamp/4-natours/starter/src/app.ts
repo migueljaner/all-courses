f6a33f5fc2088ec86f674edba39f27dabb2246f6
Miguel Janer <migueljanermudoy@gmail.com>
2024-01-10 15:47:57 +0100

    Add express-mongo-sanitize, xss-clean, and hpp middleware

--- complete-node-bootcamp/4-natours/starter/src/app.ts
+++ complete-node-bootcamp/4-natours/starter/src/app.ts
@@ -6,6 +6,11 @@ import tourRouter from './routes/tourRoutes';
 import userRouter from './routes/userRoutes';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+//@ts-expect-error
+import xss from 'xss-clean';
+//@ts-expect-error
+import hpp from 'hpp';
 
 const app = express();
 
@@ -29,6 +34,26 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'maxGroupSize',
+      'price',
+    ],
+  })
+);
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
